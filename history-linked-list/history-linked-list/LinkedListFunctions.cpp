b12f89aacb4d6b6e97ab598e5415b3609a391a29
Ivan Staev <IDStaev19@codingburgas.bg>
2022-02-03 19:27:58 +0200

    Add linked list functions

---
+++ history-linked-list/history-linked-list/LinkedListFunctions.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include "linkedList.h"
+#include "LinkedListFunctions.h"
+using namespace std;
+
+void displayList(Node* node)
+{
+	while (node != NULL)
+	{
+		cout << node->getYear() << ", ";
+		cout << node->getName() << ", ";
+		cout << node->getInfo() << endl;
+
+		node = node->getNext();
+	}
+}
+
+void insertFirstNode(Node** head, string y, string n, string d)
+{
+	Node* newNode = new Node(y, n, d);
+	newNode->setNext(*head);
+	*head = newNode;
+}
+
+void insertLastNode(Node** head, string y, string n, string d)
+{
+	Node* newNode = new Node(y, n, d);
+
+	if (*head == NULL)
+	{
+		*head = newNode;
+		return;
+	}
+
+	Node* tail = *head;
+	while (tail->getNext() != NULL)
+	{
+		tail = tail->getNext();
+	}
+
+	tail->setNext(newNode);
+}
+
+void insertAt(Node* prev, string y, string n, string d)
+{
+	if (prev == NULL)
+	{
+		cout << "Can't insert\n";
+		return;
+	}
+
+	Node* newNode = new Node(y, n, d);
+
+	newNode->setNext(prev->getNext());
+	prev->setNext(newNode);
+}
---
+++ history-linked-list/history-linked-list/LinkedListFunctions.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+#include "LinkedList.h"
+using namespace std;
+
+void displayList(Node* node);
+void insertFirstNode(Node** head, string y, string n, string d);
+void insertLastNode(Node** head, string y, string n, string d);
+void insertAt(Node* prev, string y, string n, string d);
