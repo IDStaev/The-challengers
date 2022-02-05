3c017955d455b3bac5a229b36260e836910c7d46
Ivan Staev <IDStaev19@codingburgas.bg>
2022-02-05 13:36:06 +0200

    Add comments to LinkedList.cpp

--- history-linked-list/history-linked-list/LinkedList.cpp
+++ history-linked-list/history-linked-list/LinkedList.cpp
@@ -3,49 +3,60 @@
 #include "linkedList.h"
 using namespace std;
 
+//Constructor to the class
 Node::Node(string year, string name, string info)
 {
-	this->year = year;
-	this->name = name;
-	this->info = info;
-	next = NULL;
+	setYear(year);
+	setName(name);
+	setInfo(info);
+
+	//Automatically assigns NULL to next
+	setNext(NULL);
 }
 
+//Get the year of an object
 string Node::getYear()
 {
 	return year;
 }
 
+//Get the name of an object
 string Node::getName()
 {
 	return name;
 }
 
+//Get the info of an object
 string Node::getInfo()
 {
 	return info;
 }
 
+//Get the next node of a linked list
 Node* Node::getNext()
 {
 	return next;
 }
 
+//Set the year of an object
 void Node::setYear(string year)
 {
 	this->year = year;
 }
 
+//Set the name of an object
 void Node::setName(string name)
 {
 	this->name = name;
 }
 
+//Set the info of an object
 void Node::setInfo(string info)
 {
 	this->info = info;
 }
 
+//Set the next node of a linked list
 void Node::setNext(Node* nextNode)
 {
 	next = nextNode;
